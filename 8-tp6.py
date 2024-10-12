60593c1a743c731f9d6a1675f1fe7a12cb9c14cc
BautistaTordini <bautistatordini.uns@gmail.com>
2024-10-12 13:13:19 -0300

    git ignore ejectado

---
+++ 8-tp6.py
@@ -0,0 +1,17 @@
+# 8) Un organizador de eventos nos pide diseñar un sistema simple para gestionar los 
+# eventos. El sistema debe permitir registrar eventos, participantes y organizadores, y 
+# asignar participantes a eventos y organizadores a eventos. 
+# Requerimientos 
+# Evento: Cada evento tiene un nombre, una fecha y una descripción. Los eventos 
+# pueden tener múltiples participantes y un organizador asignado. 
+
+# Participante: Cada participante tiene un nombre, una dirección de correo electrónico 
+# y un número de teléfono. Los participantes pueden registrarse en uno o más 
+# eventos. 
+# Organizador: Cada organizador tiene un nombre, una dirección de correo electrónico 
+# y una especialidad (por ejemplo, planificación de eventos, catering, músico, DJ, etc.). 
+# Cada organizador puede estar a cargo de uno o más eventos. 
+# A. Crea un diagrama de clases que incluya tres clases principales: Evento, 
+# Participante y Organizador. 
+# B. Implementa las clases en python 
+# C.  Crea una clase tester para cada clase implementada
