e88b75fbe363a39774592b68deb6c0e160977fe3
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-13 18:48:04 -0300

    update

--- TP8/EJERCICIO-1-tp8/main.py
+++ TP8/EJERCICIO-1-tp8/main.py
@@ -1,12 +1,10 @@
+# main.py
 import json
-
-from clases.libro import libro
-
 
 class Tester:
     @staticmethod
     def cargar_libros():
-        # Abre el archivo 'libros.json' en modo lectura
+        # Abre el archivo 'data/libros.json' en modo lectura
         with open('libros.json', 'r', encoding='utf-8') as file:
             # Carga los datos del archivo JSON en una lista de diccionarios
             data = json.load(file)
@@ -27,20 +25,17 @@ class Tester:
         return resultados
 
 if __name__ == "__main__":
-    # Carga los libros desde el archivo 'libros.json'
+    # Carga los libros desde el archivo 'data/libros.json'
     libros = Tester.cargar_libros()
     # Muestra todos los libros cargados
     Tester.mostrar_libros(libros)
     
     # Solicita al usuario ingresar un año de publicación para buscar libros
     ano = int(input("Ingrese el año de publicación para buscar libros: "))
-    # Busca libros cuyo año de publicación coincide con el año ingresado
     resultados = Tester.buscar_por_ano(libros, ano)
     
     if resultados:
-        # Si se encontraron libros, los muestra
         print("Libros encontrados:")
         Tester.mostrar_libros(resultados)
     else:
-        # Si no se encontraron libros, muestra un mensaje de error
         print("No se encontraron libros para el año ingresado.")
