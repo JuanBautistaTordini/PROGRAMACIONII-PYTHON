a2fdf92eba57f6180ec8010d92e5d9272a7e79b7
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-13 18:26:50 -0300

    coment - libro.py

--- TP8/EJERCICIO-1-tp8/class/Libro.py
+++ TP8/EJERCICIO-1-tp8/class/Libro.py
@@ -1,13 +1,16 @@
 import json
 
 class Libro:
+    # Constructor de la clase Libro
     def __init__(self, titulo: str, autor: str, genero: str, ano_publicacion: int, isbn: int):
+        # Inicializa los atributos privados de la clase
         self.__titulo = titulo
         self.__autor = autor
         self.__genero = genero
         self.__ano_publicacion = ano_publicacion
         self.__isbn = isbn
     
+    # Método para convertir el objeto Libro a un diccionario
     def toDiccionario(self):
         return {
             "Título": self.__titulo,
@@ -17,6 +20,7 @@ class Libro:
             "ISBN": self.__isbn,
         }
     
+    # Método de clase para crear un objeto Libro a partir de un diccionario
     @classmethod
     def fromDiccionario(cls, dicc):
         return cls(
