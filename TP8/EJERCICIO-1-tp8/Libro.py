b0958f0839ac974840d06afc340d4c34edde4080
BautistaTordini <bautistatordini.uns@gmail.com>
2024-11-14 01:09:54 -0300

    chore: Refactor Libro class to adhere to PEP8 naming conventions

--- TP8/EJERCICIO-1-tp8/Libro.py
+++ TP8/EJERCICIO-1-tp8/Libro.py
@@ -1,4 +1,38 @@
+'''
+Diagrama clase Libro:
++----------------------------------------+
+|               Libro                    |
++----------------------------------------+
+| - titulo: str                          |
+| - autor: str                           |
+| - genero: str                          |
+| - ano_publicacion: int                 |
+| - isbn: int                            |
++----------------------------------------+
+| + get_titulo(): str                    |
+| + get_autor(): str                     |
+| + get_genero(): str                    |
+| + get_ano_publicacion(): int           |
+| + get_isbn(): int                      |
+| + toDiccionario(): dict                |
+| + fromDiccionario(dicc: dict): Libro   |
++----------------------------------------+
+
+'''
+
+
 class Libro:
+    """
+    Clase que representa un libro.
+
+    Atributos:
+    - titulo (str): El título del libro.
+    - autor (str): El autor del libro.
+    - genero (str): El género del libro.
+    - ano_publicacion (int): El año de publicación del libro.
+    - isbn (int): El número ISBN del libro.
+    """
+
     def __init__(self, titulo: str, autor: str, genero: str, ano_publicacion: int, isbn: int):
         self.titulo = titulo
         self.autor = autor
@@ -23,7 +57,15 @@ class Libro:
         return self.isbn
     
     # Método para convertir el objeto Libro a un diccionario
+    
+    
     def toDiccionario(self):
+        """
+        Convierte el objeto Libro a un diccionario.
+
+        Returns:
+        dict: Un diccionario que representa el objeto Libro.
+        """
         return {
             "Título": self.titulo,
             "Autor": self.autor,
@@ -34,6 +76,15 @@ class Libro:
     
     @classmethod
     def fromDiccionario(cls, dicc):
+        """
+        Crea un objeto Libro a partir de un diccionario.
+
+        Args:
+        dicc (dict): Un diccionario que contiene los atributos del libro.
+
+        Returns:
+        Libro: Un objeto Libro creado a partir del diccionario.
+        """
         return cls(
             dicc["titulo"],
             dicc["autor"],
